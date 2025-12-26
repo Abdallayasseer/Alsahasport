4918903ab1911ac49cb76f42de4bb794bf64bcb0
Abdullah Yasser <2302939@student.eelu.edu.eg>
2025-12-26 20:42:47 +0200

    Protect Admin Route

--- src/routes/admin.route.js
+++ src/routes/admin.route.js
@@ -1,35 +1,29 @@
 const express = require("express");
 const router = express.Router();
-const { 
-  createCode, 
-  addChannel, 
-  getAllCodes,     
-  deleteCode,      
-  getLiveSessions, 
-  addProvider,     
-  loginAdmin 
-} = require('../controllers/admin.Controller');
-
-// Codes Management
-router.post('/codes', createCode);
-router.get('/codes', getAllCodes);         
-router.delete('/code/:id', deleteCode);    
-
-// Monitoring
-router.get('/sessions/live', getLiveSessions)
-
-// Content Management
-router.post('/channels', addChannel);
-router.post('/provider', addProvider);     
-const { protectAdmin } = require("../middlewares/authMiddleware");
+const { protectAdmin } = require("../middlewares/authMiddleware"); 
+const {
+  createCode,
+  addChannel,
+  getAllCodes,
+  deleteCode,
+  getLiveSessions,
+  addProvider,
+  loginAdmin,
+} = require("../controllers/admin.Controller");
 
 router.post("/login", loginAdmin);
 
 router.use(protectAdmin);
 
+router.post("/codes", createCode);
+router.get("/codes", getAllCodes);
+router.delete("/code/:id", deleteCode);
 
-// router.get('/codes', getAllCodes);
-// router.delete('/codes/:id', deleteCode);
-// router.get('/online-users', getOnlineUsers);
+// Monitoring
+router.get("/sessions/live", getLiveSessions);
+
+// Content Management
+router.post("/channels", addChannel);
+router.post("/provider", addProvider);
 
 module.exports = router;
